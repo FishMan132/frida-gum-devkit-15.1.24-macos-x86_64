40103e7be2380fd1447beb7a374a1b6a4fb5538a
yin.zhu <yin.zhu@kuaishou.com>
2022-06-10 16:53:39 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# frida-gum-devkit-15.1.24-macos-x86_64
---
+++ frida-gum-example.c
@@ -0,0 +1,162 @@
+/*
+ * Compile with:
+ *
+ * clang -fPIC frida-gum-example.c -o frida-gum-example -L. -lfrida-gum -lresolv -lm -isysroot "$(xcrun --sdk macosx --show-sdk-path)" -arch x86_64 -Wl,-dead_strip
+ *
+ * Visit https://frida.re to learn more about Frida.
+ */
+
+#include "frida-gum.h"
+
+#include <fcntl.h>
+#include <unistd.h>
+
+typedef struct _ExampleListener ExampleListener;
+typedef enum _ExampleHookId ExampleHookId;
+
+struct _ExampleListener
+{
+  GObject parent;
+
+  guint num_calls;
+};
+
+enum _ExampleHookId
+{
+  EXAMPLE_HOOK_OPEN,
+  EXAMPLE_HOOK_CLOSE
+};
+
+static void example_listener_iface_init (gpointer g_iface, gpointer iface_data);
+
+#define EXAMPLE_TYPE_LISTENER (example_listener_get_type ())
+G_DECLARE_FINAL_TYPE (ExampleListener, example_listener, EXAMPLE, LISTENER, GObject)
+G_DEFINE_TYPE_EXTENDED (ExampleListener,
+                        example_listener,
+                        G_TYPE_OBJECT,
+                        0,
+                        G_IMPLEMENT_INTERFACE (GUM_TYPE_INVOCATION_LISTENER,
+                            example_listener_iface_init))
+
+
+//GObject测试
+// G_DEFINE_TYPE (PMDList, pm_dlist, G_TYPE_OBJECT)
+// static
+// void pm_dlist_init (PMDList *self)
+// {
+//     g_print ("enter pm_dlist_init\n");
+// }
+
+// static
+// void pm_dlist_class_init (PMDListClass *klass)
+// {
+//     g_print ("enter pm_dlist_class_init!\n");
+// }
+
+static void
+example_follow_me_should_support_nullable_event_sink (void)
+{
+    g_setenv("GOBJECT_DEBUG","true",true);
+    // gobject_init();
+    gum_init_embedded ();//很重要
+    GumStalker * stalker = gum_stalker_new();
+    g_print("stalker: %p \n", stalker);
+    gum_stalker_follow_me (stalker, NULL, NULL);
+    gpointer p = malloc (1);
+    free (p);
+    gum_stalker_unfollow_me (stalker);
+}
+
+int
+main (int argc,
+      char * argv[])
+{
+//   GumInterceptor * interceptor;
+//   GumInvocationListener * listener;
+
+//   gum_init_embedded ();
+
+//   interceptor = gum_interceptor_obtain ();
+//   listener = g_object_new (EXAMPLE_TYPE_LISTENER, NULL);
+
+//   gum_interceptor_begin_transaction (interceptor);
+//   gum_interceptor_attach (interceptor,
+//       GSIZE_TO_POINTER (gum_module_find_export_by_name (NULL, "open")),
+//       listener,
+//       GSIZE_TO_POINTER (EXAMPLE_HOOK_OPEN));
+//   gum_interceptor_attach (interceptor,
+//       GSIZE_TO_POINTER (gum_module_find_export_by_name (NULL, "close")),
+//       listener,
+//       GSIZE_TO_POINTER (EXAMPLE_HOOK_CLOSE));
+//   gum_interceptor_end_transaction (interceptor);
+
+//   close (open ("/etc/hosts", O_RDONLY));
+//   close (open ("/etc/fstab", O_RDONLY));
+
+//   g_print ("[*] listener got %u calls\n", EXAMPLE_LISTENER (listener)->num_calls);
+
+//   gum_interceptor_detach (interceptor, listener);
+
+//   close (open ("/etc/hosts", O_RDONLY));
+//   close (open ("/etc/fstab", O_RDONLY));
+
+//   g_print ("[*] listener still has %u calls\n", EXAMPLE_LISTENER (listener)->num_calls);
+
+//   g_object_unref (listener);
+//   g_object_unref (interceptor);
+
+//   gum_deinit_embedded ();
+
+//   gpointer p;
+
+  example_follow_me_should_support_nullable_event_sink();
+  return 0;
+}
+
+static void
+example_listener_on_enter (GumInvocationListener * listener,
+                           GumInvocationContext * ic)
+{
+  ExampleListener * self = EXAMPLE_LISTENER (listener);
+  ExampleHookId hook_id = GUM_IC_GET_FUNC_DATA (ic, ExampleHookId);
+
+  switch (hook_id)
+  {
+    case EXAMPLE_HOOK_OPEN:
+      g_print ("[*] open(\"%s\")\n", (const gchar *) gum_invocation_context_get_nth_argument (ic, 0));
+      break;
+    case EXAMPLE_HOOK_CLOSE:
+      g_print ("[*] close(%d)\n", GPOINTER_TO_INT (gum_invocation_context_get_nth_argument (ic, 0)));
+      break;
+  }
+
+  self->num_calls++;
+}
+
+static void
+example_listener_on_leave (GumInvocationListener * listener,
+                           GumInvocationContext * ic)
+{
+}
+
+static void
+example_listener_class_init (ExampleListenerClass * klass)
+{
+  (void) EXAMPLE_IS_LISTENER;
+  (void) glib_autoptr_cleanup_ExampleListener;
+}
+
+static void
+example_listener_iface_init (gpointer g_iface,
+                             gpointer iface_data)
+{
+  GumInvocationListenerInterface * iface = g_iface;
+
+  iface->on_enter = example_listener_on_enter;
+  iface->on_leave = example_listener_on_leave;
+}
+
+static void
+example_listener_init (ExampleListener * self)
+{
+}
